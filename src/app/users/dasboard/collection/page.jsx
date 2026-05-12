d750084292c2298f3c1ff670d1f0efd0e7a59476
rkyarc <arickyh1@gmail.com>
2026-05-13 04:15:07 +0700

    first commit

--- src/app/users/dasboard/collection/page.jsx
+++ src/app/users/dasboard/collection/page.jsx
@@ -1,36 +1,35 @@
 import Header from "@/components/Dasboard/Header";
+import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
 import Link from "next/link";
+import prisma from "@/libs/prisma";
 
-const Page = () => {
+const Page = async () => {
+  const user = await authUserSession;
+  const collection = await prisma.collection.findMany({
+    where: { user_email: user.email },
+  });
+
   return (
     <section className="mt-4 w-full px-4">
-      <Header title={"My Collection"}/>
+      <Header title={"My Collection"} />
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <Link href="/" className="relative border-2 border-color-accent">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-color-accent">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-color-accent">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-color-accent">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-            <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
-          </div>
-        </Link>
+        {collection.map((collect, index) => {
+          return (
+            <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative">
+              <Image
+                src={collect.anime_image}
+                alt={collect.anime_image}
+                width={350}
+                height={350}
+                className="w-full"
+              />
+              <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
+                <h5 className="text-xl text-center">{collect.anime_title}</h5>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/app/users/dasboard/comment/page.js
@@ -0,0 +1,34 @@
+import Header from "@/components/Dasboard/Header";
+import { authUserSession } from "@/libs/auth-libs";
+import prisma from "@/libs/prisma";
+import Link from "next/link";
+import React from "react";
+
+const Page = async () => {
+  const user = await authUserSession();
+  const comments = await prisma.comment.findMany({
+    where: { user_email: user.email },
+  });
+
+  return (
+    <section className="mt-4 w-full px-4">
+      <Header title={"My Comment"} />
+      <div className="grid grid-cols-1 py-2 gap-4">
+        {comments.map((comment) => {
+          return (
+            <Link
+              href={`/anime/${comment.anime_mal_id}`}
+              key={comment.id}
+              className="bg-color-primary text-color-dark p-4"
+            >
+              <p className="text-sm">{comment.anime_title}</p>
+              <p className="italic">{comment.comment}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
