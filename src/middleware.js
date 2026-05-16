303c6287bfb7ba9a1342cd3cc00104b54840479d
rkyarc <arickyh1@gmail.com>
2026-05-16 14:08:17 +0700

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,15 @@
 // validasi route user
-export { default } from "next-auth/middleware"
-export const config = { matcher: ["/users/:path*"] }
+// export { default } from "next-auth/middleware"
+// export const config = { matcher: ["/users/:path*"] }
+
+// import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+})
+
+export const config = {
+  matcher: ["/users/:path*"],
+}
